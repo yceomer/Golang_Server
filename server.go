5933534301e33bc9949013546c4cd6b0958421b2
yceomer <54587821+yceomer@users.noreply.github.com>
2022-01-21 15:27:28 +0300

    Golang_Web_Server
    This is a web server with golang.

---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
+
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":5555", nil)
+
+	fmt.Println("Web Server")
+
+}
